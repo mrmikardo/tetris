c9af1596971a6db829d816dbc8e6f07cae45b3f8
Jack Westmore <jackwestmore@gmail.com>
2021-07-25 18:20:02 +0100

    Add basic game engine
    This commit adds handling for falling tetromino blocks, and updates the
    state (filled blocks, current tetromino position) accordingly, when the
    tetromino makes contact with the current base.

--- src/tetris/db.cljs
+++ src/tetris/db.cljs
@@ -2,4 +2,18 @@
 
 (def default-db
   {:timer-interval-id nil
-   :elapsed-game-time 0})
+   :elapsed-game-time 0
+   :playfield {
+               :active-tetronomion-coords [[1 0] [2 0] [1 -1] [2 -1]]
+               ;; to start with, base is the bottom row
+               :base-coords [[0 15]
+                             [1 15]
+                             [2 15]
+                             [3 15]
+                             [4 15]
+                             [5 15]
+                             [6 15]
+                             [7 15]
+                             [8 15]
+                             [9 15]]
+               }})
--- src/tetris/events.cljs
+++ src/tetris/events.cljs
@@ -9,11 +9,46 @@
  (fn [_ _]
    db/default-db))
 
+(defn- update-active-tetronomion
+  "Move the currently falling tetronomion one coord closer to the base."
+  [coords]
+  (map #(vector (first %) (+ 1 (second %))) coords))
+
+(defn- contiguous-with-base?
+  "A tetronomion is contiguous with the base if it shares at least a single
+  edge with one of the edges of the base (i.e. 2 coords)."
+  [tetronomion-coords base-coords]
+  (>=
+      (count
+       (clojure.set/intersection
+        (set tetronomion-coords)
+        (set base-coords)))
+      2))
+
+(defn- update-playfield [playfield]
+  (let [{:keys [active-tetronomion-coords base-coords]} playfield]
+    (if (contiguous-with-base? active-tetronomion-coords base-coords)
+      (-> playfield
+        ;; wipe active tetronomion piece
+        (dissoc :active-tetronomion-coords)
+        ;; update base coords to include tetronomion piece
+        (assoc :base-coords (concat base-coords active-tetronomion-coords))
+        ;; TODO set up next tetronomion to fall from the sky...
+        )
+      (assoc playfield :active-tetronomion-coords (update-active-tetronomion active-tetronomion-coords)))))
+
+;; handle clock events
 (re-frame/reg-event-db
  ::game-timer
  (fn [db [_ new-time-value]]
-   (assoc db :elapsed-game-time new-time-value)))
+   (-> db
+       ;; update the clock
+       (assoc :elapsed-game-time new-time-value)
+       ;; update the playfield
+       (assoc-in [:playfield] (update-playfield (get-in db [:playfield]))))))
 
+;; set the ID of the DOM interval timer
+;; we'll need this later to clear the state (stop the game)
 (re-frame/reg-event-db
  ::set-timer-interval-id
  (fn [db [_ interval-id]]
