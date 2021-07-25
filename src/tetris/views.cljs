91281ee2a4ebfa0e5d4c426ed3a55c05b8af978f
Jack Westmore <jackwestmore@gmail.com>
2021-07-25 11:25:42 +0100

    basic playfield layout

--- src/tetris/views.cljs
+++ src/tetris/views.cljs
@@ -1,21 +1,38 @@
 (ns tetris.views
   (:require
    [re-frame.core :as re-frame]
+   [reagent.core :as r]
    [tetris.subs :as subs]
    ))
 
-;; this is a Reagent "form 1" component
-(defn- demo-button []
-  [:button
-   {:type "button"
-    :title "To be clicked"
-    :on-click #(js/console.log "He clicked me! :O")}
-   "Click me :)"])
+(defn playfield []
+  (let [rows-count 16
+        cols-count 10
+        cell-size  35]
+    [:div {:class ["relative"]}
+     [:table
+      {:class ["table-fixed border-2 border-purple-700"]
+       :style {:width (* cell-size cols-count)}}
+      [:tbody
+       (for [i (range rows-count)]
+         ^{:key i}
+         [:tr
+          {:style {:height cell-size}}
+          (for [j (range cols-count)]
+            ^{:key j}
+            [:td
+             {:class ["p-0 border border-purple-400 text-center select-none"]}])])]]]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1
-      "Hello from " @name]
-     [demo-button]
+     {:class "container"}
+     [:div
+      {:class ["row" "flex" "justify-center" "mt-20"]}
+      [:div
+       {:class ["flex" "flex-col" "items-center"]}
+       [:h1
+        {:class ["mb-2"]}
+        "Tetris"]
+       [playfield]]]
      ]))
