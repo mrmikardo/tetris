49dacaf3436da5fcc64b25ac84b40b42e2754b37
Jack Westmore <jackwestmore@gmail.com>
2021-07-28 22:20:48 +0100

    core: rename imports to terser variants

--- src/tetris/core.cljs
+++ src/tetris/core.cljs
@@ -1,7 +1,7 @@
 (ns tetris.core
   (:require
    [reagent.dom :as rdom]
-   [re-frame.core :as re-frame]
+   [re-frame.core :as rf]
    [tetris.events :as events]
    [tetris.views :as views]
    [tetris.config :as config]
@@ -13,12 +13,12 @@
     (println "dev mode")))
 
 (defn ^:dev/after-load mount-root []
-  (re-frame/clear-subscription-cache!)
+  (rf/clear-subscription-cache!)
   (let [root-el (.getElementById js/document "app")]
     (rdom/unmount-component-at-node root-el)
     (rdom/render [views/main-panel] root-el)))
 
 (defn init []
-  (re-frame/dispatch-sync [::events/initialize-db])
+  (rf/dispatch-sync [::events/initialize-db])
   (dev-setup)
   (mount-root))
